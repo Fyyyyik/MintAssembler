cc3afec2820e935a966f23ecce3beca5a07bc1a6
Fyik <maximevary57@gmail.com>
2026-05-16 12:37:28 +0200

    Create symbols for semantic

---
+++ Mint/Semantics/Symbols.cs
@@ -0,0 +1,29 @@
+﻿using Mint.AstNodes;
+
+namespace Mint.Semantics
+{
+    public record ModuleSymbol
+    {
+        public Dictionary<string, ClassSymbol> Classes { get; } = new();
+    }
+
+    public record ClassSymbol
+    {
+        public required string Name;
+        public Dictionary<string, VariableSymbol> Variables { get; } = new();
+        public Dictionary<string, FunctionSymbol> Functions { get; } = new();
+    }
+
+    public record VariableSymbol
+    {
+        public required string Name { get; init; }
+        public required TypeNode Type { get; init; }
+    }
+
+    public record FunctionSymbol
+    {
+        public required string Name { get; init; }
+        public required TypeNode ReturnType { get; init; }
+        public List<ParamNode> Parameters { get; init; } = new();
+    }
+}
