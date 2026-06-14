48dacfcf96ca6e78cdb4039bdb294edbdf4ec265
Fyik <maximevary57@gmail.com>
2026-06-14 11:56:36 +0200

    Add OneOf package

---
+++ Mint/CodeGenerators/CodeGenerator.cs
@@ -0,0 +1,14 @@
+﻿using Mint.AstNodes;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Mint.CodeGenerators
+{
+    public abstract class CodeGenerator
+    {
+        private ModuleNode _module;
+
+        public CodeGenerator(ModuleNode module) => _module = module;
+    }
+}
